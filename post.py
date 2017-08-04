3240210a8e6ebe416e745033b698f48d12717a76
ehdgks0627 <ehdgks0627@naver.com>
2017-08-04 20:43:19 +0900

    운송장 조회 기능 추가

--- post.py
+++ post.py
@@ -40,4 +40,22 @@ while True:
         url = "http://info.sweettracker.co.kr/api/v1/trackingInfo?t_key=%s&t_code=%s&t_invoice=%s" % (api_key, t_code, t_invoice)
         r = s.get(url)
         data = json.loads(r.text)
-        print(data)
+        print("[배송 정보]")
+        try:
+            print("보낸사람 : %s" % (data["senderName"]))
+            print("받는사람 : %s" % (data["receiverName"]))
+            print("상품명 : %s" % (data["itemName"]))
+            print("받는사람 주소 : %s" % (data["receiverAddr"]))
+            print("주문 번호 : %s" % (data["orderNumber"]))
+            print("광고용 주소 : %s " % (data["adUrl"]))
+            print("배송 예정 시간 : %s" % (data["estimate"]))
+            print("상태 : %s" % ("배송중" if data["complete"] == False else "배송완료"))
+            print("수령인 정보 : %s" % (data["recipient"]))
+            print("상품 이미지 : %s" % (data["itemImage"]))
+            for info in data["trackingDetails"]:
+                print("[%s] %-10s | %-10s | %-10s" % (info["timeString"], info["kind"], info["where"], info["telno"]))
+        except KeyError:
+            try:
+                print(data["msg"])
+            except KeyError:
+                print("처리되지 않은 예외입니다")
